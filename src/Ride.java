32151fc4b3efd8d441901c8c0a13be3bfe2868f1
EpiphanyOct <144693830+EpiphanyOct@users.noreply.github.com>
2024-12-05 23:08:17 +0800

    Complete Part 4A- Collection Class (Linked List)
    Complete Part 4A- Collection Class (Linked List)

--- src/Ride.java
+++ src/Ride.java
@@ -1,12 +1,17 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Ride implements RideInterface {
     private String rideName;
     private Employee operator;
     private Queue<Visitor> queue = new LinkedList<>();
     private LinkedList<Visitor> rideHistory = new LinkedList<>();
+    private int maxRider; 
+    private int numOfCycles; 
 
     public Ride() {
     }
@@ -14,6 +19,8 @@ public class Ride implements RideInterface {
     public Ride(String rideName, Employee operator) {
         this.rideName = rideName;
         this.operator = operator;
+        this.maxRider = 7; 
+        this.numOfCycles = 0;
     }
 
     @Override
@@ -43,12 +50,11 @@ public class Ride implements RideInterface {
             return;
         }
         System.out.println("Running one cycle of " + rideName);
-        // Assuming maxRider is set to the maximum number of riders for one cycle
-        int maxRider = 4; // This value should be set according to the specific ride
         for (int i = 0; i < maxRider && !queue.isEmpty(); i++) {
             Visitor visitor = queue.poll();
             addVisitorToHistory(visitor);
         }
+        numOfCycles++;
     }
 
     @Override
@@ -73,6 +79,27 @@ public class Ride implements RideInterface {
         while (iterator.hasNext()) {
             Visitor visitor = iterator.next();
             System.out.println(visitor.getName());
+        }
+    }
+
+    public void sortRideHistory() {
+        Collections.sort(rideHistory, new Comparator<Visitor>() {
+            @Override
+            public int compare(Visitor v1, Visitor v2) {
+                int nameCompare = v1.getName().compareTo(v2.getName());
+                if (nameCompare != 0) {
+                    return nameCompare;
+                }
+                return Integer.compare(v1.getAge(), v2.getAge());
+            }
+        });
+    }
+
+    public void printSortedRideHistory() {
+        System.out.println("Sorted Ride History:");
+        sortRideHistory();
+        for (Visitor visitor : rideHistory) {
+            System.out.println(visitor.getName() + ", Age: " + visitor.getAge());
         }
     }
 
@@ -107,5 +134,21 @@ public class Ride implements RideInterface {
 
     public void setRideHistory(LinkedList<Visitor> rideHistory) {
         this.rideHistory = rideHistory;
+    }
+
+    public int getMaxRider() {
+        return maxRider;
+    }
+
+    public void setMaxRider(int maxRider) {
+        this.maxRider = maxRider;
+    }
+
+    public int getNumOfCycles() {
+        return numOfCycles;
+    }
+
+    public void setNumOfCycles(int numOfCycles) {
+        this.numOfCycles = numOfCycles;
     }
 }
